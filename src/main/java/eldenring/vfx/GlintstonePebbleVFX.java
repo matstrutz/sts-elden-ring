5d66030180806525d3121daecdf5f2c7a9d4cec8
matstrutz <tuffaogamer@hotmail.com>
2023-07-22 19:08:25 -0300

    feat: test animation

--- src/main/java/eldenring/vfx/GlintstonePebbleVFX.java
+++ src/main/java/eldenring/vfx/GlintstonePebbleVFX.java
@@ -1,10 +1,14 @@
 package eldenring.vfx;
 
 import basemod.helpers.VfxBuilder;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import eldenring.util.TextureLoader;
+
+import java.util.function.BiFunction;
 
 import static basemod.helpers.VfxBuilder.Interpolations.*;
 
@@ -15,15 +19,21 @@ public class GlintstonePebbleVFX {
     //TODO CORRECT ANIMATION WITH SOUND OF DAMAGE
     //TODO FINISH ANIMATION
     public AbstractGameEffect animation(AbstractCreature from, AbstractCreature to){
+        BiFunction<Float, Float, AbstractGameEffect> tail = (x, y) -> new VfxBuilder(BALL, x, y, 0.6F)
+                .setScale(0.6F)
+                .moveX(x, to.hb.cX)
+                .moveY(y , to.hb.cY)
+                .fadeOut(0.2F)
+                .build();
 
         return new VfxBuilder(BALL, 0.6F)
-                .scale(0.1F, 1.5F, BOUNCE)
+                .scale(0.1F, 1F, BOUNCE)
                 .setX(from.hb.cX - (from.hb.width / 2))
-                .setY(from.hb.cY + (from.hb.height / 2))
+                .setY(from.hb.cY + (from.hb.height / 4))
                 .andThen(1F)
                 .moveX(from.hb.cX - (from.hb.width / 2), to.hb.cX)
-                .moveY(from.hb.cY + (from.hb.height / 2) , to.hb.cY)
+                .moveY(from.hb.cY + (from.hb.height / 4) , to.hb.cY)
+                .emitEvery(tail, 0.1F)
                 .build();
     }
-
 }
