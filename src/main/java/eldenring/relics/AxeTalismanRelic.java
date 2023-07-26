be9abb158b27694aff31de139cc8e09c45c97d6b
matstrutz <tuffaogamer@hotmail.com>
2023-07-25 23:17:42 -0300

    feat: new relic and fix old relics

--- src/main/java/eldenring/relics/AxeTalismanRelic.java
+++ src/main/java/eldenring/relics/AxeTalismanRelic.java
@@ -7,7 +7,7 @@ import static eldenring.EldenRingSTS.makeID;
 public class AxeTalismanRelic extends BaseRelic {
     private static final String NAME = "AxeTalisman";
     public static final String ID = makeID(NAME);
-    private static final RelicTier RARITY = RelicTier.UNCOMMON;
+    private static final RelicTier RARITY = RelicTier.RARE;
     private static final LandingSound SOUND = LandingSound.SOLID;
     private static final int DAMAGE_PERCENT = 50;
 
--- src/main/java/eldenring/relics/DaedicarsWoeRelic.java
+++ src/main/java/eldenring/relics/DaedicarsWoeRelic.java
@@ -13,14 +13,25 @@ public class DaedicarsWoeRelic extends BaseRelic {
     private static final LandingSound SOUND = LandingSound.SOLID;
     private static final int STR_GAIN = 1;
 
+    private boolean firstTurn = true;
+
     public DaedicarsWoeRelic() {
         super(ID, NAME, RARITY, SOUND);
     }
 
     @Override
+    public void atPreBattle() {
+        this.firstTurn = true;
+    }
+
+    @Override
     public void atTurnStart() {
-        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
-            this.addToTop(new ApplyPowerAction(m, m, new StrengthPower(m, STR_GAIN), STR_GAIN));
+        if(this.firstTurn){
+            this.flash();
+            for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+                this.addToTop(new ApplyPowerAction(m, m, new StrengthPower(m, STR_GAIN), STR_GAIN));
+            }
+            this.firstTurn = false;
         }
     }
 
