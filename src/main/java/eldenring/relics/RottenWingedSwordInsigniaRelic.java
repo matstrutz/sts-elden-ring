625b9c3fc3f84b5280663b375bed7b6b34b80cf7
matstrutz <tuffaogamer@hotmail.com>
2023-10-06 00:12:03 -0300

    feat: fix all relics related to multi attacks

--- src/main/java/eldenring/relics/RottenWingedSwordInsigniaRelic.java
+++ src/main/java/eldenring/relics/RottenWingedSwordInsigniaRelic.java
@@ -1,6 +1,7 @@
 package eldenring.relics;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -15,23 +16,50 @@ public class RottenWingedSwordInsigniaRelic extends BaseRelic {
     private static final RelicTier RARITY = RelicTier.UNCOMMON;
     private static final LandingSound SOUND = LandingSound.CLINK;
     private static final int STR = 2;
+    private static AbstractCard lastCard;
+    private static final int ATTACK_COUNT = 3;
 
     public RottenWingedSwordInsigniaRelic() {
         super(ID, NAME, RARITY, SOUND);
     }
 
     @Override
-    public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
-        if(AbstractDungeon.player.hasRelic(this.relicId)){
-            if(AbstractCard.CardType.ATTACK.equals(targetCard.type) && Objects.nonNull(targetCard.multiDamage) && targetCard.multiDamage.length > 0){
-                this.flash();
-                this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STR), STR));
+    public void onUseCard(AbstractCard card, UseCardAction useCardAction) {
+        if (this.counter == 0) {
+            if(AbstractCard.CardType.ATTACK.equals(card.type)){
+                ++this.counter;
+                lastCard = card;
+            }
+        } else {
+            if (AbstractCard.CardType.ATTACK.equals(card.type)) {
+                if (card.cost == lastCard.cost) {
+                    ++this.counter;
+                    if (this.counter >= ATTACK_COUNT) {
+                        this.flash();
+                        this.counter = 0;
+                        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                        this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STR), STR));
+                    }
+                } else {
+                    this.counter = 0;
+                    lastCard = null;
+                }
             }
         }
     }
 
     @Override
+    public void atTurnStart() {
+        this.counter = 0;
+    }
+
+    @Override
+    public void onVictory() {
+        this.counter = -1;
+    }
+
+    @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0] + STR + DESCRIPTIONS[1];
+        return DESCRIPTIONS[0] + ATTACK_COUNT + DESCRIPTIONS[1] + STR + DESCRIPTIONS[2];
     }
 }
--- src/main/java/eldenring/relics/WingedSwordInsigniaRelic.java
+++ src/main/java/eldenring/relics/WingedSwordInsigniaRelic.java
@@ -1,8 +1,12 @@
 package eldenring.relics;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 
@@ -15,24 +19,51 @@ public class WingedSwordInsigniaRelic extends BaseRelic {
     public static final String ID = makeID(NAME);
     private static final RelicTier RARITY = RelicTier.COMMON;
     private static final LandingSound SOUND = LandingSound.CLINK;
-    private static final int STR = 1;
+    private static final int STR = 2;
+    private static AbstractCard lastCard;
+    private static final int ATTACK_COUNT = 4;
 
     public WingedSwordInsigniaRelic() {
         super(ID, NAME, RARITY, SOUND);
     }
 
     @Override
-    public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
-        if(AbstractDungeon.player.hasRelic(this.relicId)){
-            if(AbstractCard.CardType.ATTACK.equals(targetCard.type) && Objects.nonNull(targetCard.multiDamage) && targetCard.multiDamage.length > 0){
-                this.flash();
-                this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STR), STR));
+    public void onUseCard(AbstractCard card, UseCardAction useCardAction) {
+        if (this.counter == 0) {
+            if(AbstractCard.CardType.ATTACK.equals(card.type)){
+                ++this.counter;
+                lastCard = card;
+            }
+        } else {
+            if (AbstractCard.CardType.ATTACK.equals(card.type)) {
+                if (card.cost == lastCard.cost) {
+                    ++this.counter;
+                    if (this.counter >= ATTACK_COUNT) {
+                        this.flash();
+                        this.counter = 0;
+                        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                        this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, STR), STR));
+                    }
+                } else {
+                    this.counter = 0;
+                    lastCard = null;
+                }
             }
         }
     }
 
     @Override
+    public void atTurnStart() {
+        this.counter = 0;
+    }
+
+    @Override
+    public void onVictory() {
+        this.counter = -1;
+    }
+
+    @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0] + STR + DESCRIPTIONS[1];
+        return DESCRIPTIONS[0] + ATTACK_COUNT + DESCRIPTIONS[1] + STR + DESCRIPTIONS[2];
     }
 }
